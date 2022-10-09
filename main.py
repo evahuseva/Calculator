2b978b5dc2541e3dbcd4b4d02950ae14beeb1ffe
evahuseva <55929630+evahuseva@users.noreply.github.com>
2022-10-10 00:54:19 +0100

    testunit

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
+import re
+
+
 class Calc:
 
-    def __init__(self):
+    def __init__(self, input_expression):
+        self.is_valid = False
         self.result = None
+        self.expression = input_expression
 
-    def calculating(self):
+    def validate(self):
+        if not isinstance(self.expression, str):
+            raise TypeError
+        self.is_valid = not re.search('^(?:0|[1-9]\\d*)(?:[+*-](?:0|[1-9]\\d*))*$', self.expression) is None
+        if not self.is_valid:
+            raise SyntaxError('Invalid input syntax.')
+
+    def calculate(self):
+        self.validate()
+        return eval(self.expression)
+
+    def output(self):
         try:
-            for index in range(len(expression) - 1):
-                if expression[index] in '*-+' and expression[index + 1] == expression[index]:
-                    print('Repeating operators in a row.')
-                    pass
-            self.result = eval(str(expression))
-            print(self.result)
-        except ZeroDivisionError:
-            print('Division by zero error.')
-            pass
-        except SyntaxError:
-            print('Invalid expression input.')
-            pass
+            return self.calculate()
+        except SyntaxError as syntax_error:
+            return syntax_error.msg
         except TypeError:
-            print('Invalid type input.')
-            pass
-        except NameError:
-            print('Invalid input.')
-            pass
-        finally:
-            pass
+            return "Wrong input type."
+        except Exception as error:
+            return f'Abnormal exit: Unexpected error has occurred ({error.msg}).'
 
 
-expression = input("Enter the expression: ")
-c1 = Calc()
-c1.calculating()
-
+c1 = Calc(input("Enter the expression: "))
+print(c1.output())
--- test_calc.py
+++ test_calc.py
@@ -1,30 +1,17 @@
-from main import Calc
 import main
+from main import Calc
 import unittest
-import re
+from unittest import mock
+from unittest.mock import patch
+from unittest import TestCase
 
 
-class MyTestCase(unittest.TestCase):
+class CalculatorTests(unittest.TestCase):
 
-    def test_invalid_type_input(self):
-        self.input_type = type(main.expression)
-        self.assertEqual(self.input_type, str)
-
-    def test_valid_input(self):
-        self.isvalid = not re.search('^(?:0|[1-9]\d*)(?:[+*-](?:0|[1-9]\d*))*$', main.expression) is None
-        self.assertEqual(self.isvalid, True)
-
-    def test_calculation(self):
-        example = Calc()
-        main.expression = '1*10-50000+200'
-        self.call_calculating = str(example.calculating())
-        self.assertEqual(self.call_calculating, '-49790')
-
-    def test_zero_division(self):
-        self.is_zero_division = False
-        if '/0' in main.expression:
-            self.is_zero_division = True
-        self.assertEqual(self.is_zero_division, False)
+    @patch('main.input', return_value='1*10-50000+200')
+    def test_multiplication(self, mock_input):
+        res = Calc.output()
+        self.assertEqual(res, '-49790')
 
 
 if __name__ == '__main__':
