3c4e74babf438a7b52f2bedab2165709e5b7a583
evahuseva <55929630+evahuseva@users.noreply.github.com>
2022-10-10 10:14:38 +0100

    testunit

--- test_calc.py
+++ test_calc.py
@@ -2,16 +2,22 @@ import main
 from main import Calc
 import unittest
 from unittest import mock
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from unittest import TestCase
+from unittest.mock import MagicMock
 
 
 class CalculatorTests(unittest.TestCase):
 
-    @patch('main.input', return_value='1*10-50000+200')
-    def test_multiplication(self, mock_input):
-        res = Calc.output()
-        self.assertEqual(res, '-49790')
+#    @patch('main.input', lambda *args: '1*10-50000+200')
+    def test_calculation1(self):
+        test1 = Calc('1*10-50000+200').output()
+#        Mock('1*10-50000+200')
+        self.assertEqual(test1, -49790)
+#
+#    def test_calculation2(self):
+#        test2 = Calc('1-1-1').output()
+#        self.assertEqual(test2, -49790)
 
 
 if __name__ == '__main__':
