f56412367f73bf592b2850b16962182c6bd43fd9
evahuseva <55929630+evahuseva@users.noreply.github.com>
2022-10-10 15:47:48 +0100

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -4,8 +4,8 @@ import pytest
 
 def test_calculation1():
     test1 = Calc('1*10-50000+200').output()
-    assert (test1, -49790)
+    assert test1, -49790
 
 
-if __name__ == '__main__':
-    pytest.main()
+if __name__ == '__calculator__':
+    pytest.calculator()
