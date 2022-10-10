be9d677f040333a224ddf08d561fb5cae61b1f63
evahuseva <55929630+evahuseva@users.noreply.github.com>
2022-10-10 12:09:43 +0100

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,14 +1,7 @@
-import main
-from main import Calc
-import unittest
+#from main import Calc
 
-
-class CalculatorTests(unittest.TestCase):
+class CalculatorTests():
 
     def test_calculation1(self):
         test1 = Calc('1*10-50000+200').output()
         self.assertEqual(test1, -49790)
-
-
-if __name__ == '__main__':
-    unittest.main()
