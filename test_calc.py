e5430cde0aaec124be1b3e1b264171561fe9fea2
evahuseva <55929630+evahuseva@users.noreply.github.com>
2022-10-10 16:28:08 +0100

    Add files via upload

--- test_calc.py
+++ test_calc.py
@@ -1,11 +1,6 @@
-from calculator import *
-import pytest
+from calculator import Calc
 
 
 def test_calculation1():
-    test1 = Calc('1*10-50000+200').output()
-    assert test1, -49790
-
-
-if __name__ == '__calculator__':
-    pytest.calculator()
+    test_value = Calc('1*10-50000+200').output()
+    assert -49790 == test_value
